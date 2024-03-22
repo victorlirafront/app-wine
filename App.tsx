cc5a2da52dec566b62ac4797dc711c0bb8cd1098
VictorLira-DEV <victorlira@wine.com.br>
2024-03-21 21:50:38 -0300

    feat: status bar

--- App.tsx
+++ App.tsx
@@ -1,18 +1,22 @@
 import { StyleSheet, View } from 'react-native';
 import PrimaryButton from './components/PrimaryButton';
+import { StatusBar } from 'expo-status-bar';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <PrimaryButton>Iniciar um ciclo</PrimaryButton>
-    </View>
+    <>
+      <StatusBar style="dark" />
+      <View style={styles.container}>
+        <PrimaryButton>Iniciar um ciclo</PrimaryButton>
+      </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ffffff',
     alignItems: 'center',
     justifyContent: 'center',
   },
