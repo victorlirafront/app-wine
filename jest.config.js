e66d5bf26c95142baabe21ac236d93182ce2ea0b
VictorLira-DEV <victorlira@wine.com.br>
2024-03-29 10:58:53 -0300

    Revert "feat: jest configuration"
    This reverts commit 78d445057465c3590806fb139252201c0fee53f1.

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  preset: 'react-native',
-  setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
-  testEnvironment: 'node',
-  transformIgnorePatterns: [
-    'node_modules/(?!(jest-)?@?react-native|@react-native-community|@react-navigation)',
-  ],
-};
