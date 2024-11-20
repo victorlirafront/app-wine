a0849e32ebb6c90949b239245736300e573a4b6e
Victor Lira <104510669+victorlirafront@users.noreply.github.com>
2024-11-20 14:10:31 -0300

    feat: add WineUpItem component

---
+++ components/WineUpItem/index.tsx
@@ -0,0 +1,51 @@
+import { View, Text, Image, StyleSheet } from 'react-native';
+import { WineUpItemProps } from './index.types';
+
+const WineUpItem = ({
+  backgroundColor = '#dbdbdb',
+  wineUpText,
+  iconImage,
+  color = '#000',
+  label,
+}: WineUpItemProps) => {
+  return (
+    <View style={styles.wrapper}>
+      <Text style={styles.label}>{label}</Text>
+      <View style={[styles.item, { backgroundColor }]}>
+        <Image source={iconImage} style={styles.iconImage} alt={'tes'} />
+        <Text style={[styles.iconText, { color }]}>{wineUpText}</Text>
+      </View>
+    </View>
+  );
+};
+
+export default WineUpItem;
+
+const styles = StyleSheet.create({
+  wrapper: {},
+  label: {
+    marginBottom: 4,
+    fontSize: 12,
+    color: '#555555',
+  },
+  item: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    borderTopLeftRadius: 4,
+    borderTopRightRadius: 4,
+    borderBottomRightRadius: 4,
+    borderBottomLeftRadius: 4,
+  },
+  iconImage: {
+    width: 16,
+    height: 16,
+    marginRight: 8,
+  },
+  iconText: {
+    fontSize: 14,
+    color: '#000',
+    fontWeight: 'bold',
+  },
+});
