dce47b4d8382fdfc980611da217e8aba3ad60681
Victor Lira <104510669+victorlirafront@users.noreply.github.com>
2024-11-20 02:19:10 -0300

    remove testing

--- __tests__/screens/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react-native';
-import App from '../../App';
-
-test('Button text', () => {
-  const { getByText } = render(<App />);
-  const textElement = getByText('Iniciar um ciclo');
-  expect(textElement).toBeDefined();
-});
